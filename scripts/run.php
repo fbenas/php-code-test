05bdd40f69e66f5d87824e61824d1ac125bdfe50
Phil Burton <phil@d3r.com>
2019-07-28 17:47:21 +0100

    Split code into testable functions.\nAdd checks to make sure each function returns a valid type; for now silently ignore errors

--- scripts/run.php
+++ scripts/run.php
@@ -7,4 +7,7 @@ use PhpCodeTest\GetBookList;
 $getBookList = new GetBookList();
 $json = $getBookList->getBooksByAuthor('Test Author', 1);
 
+$getBookList = new GetBookList('xml');
+$json = $getBookList->getBooksByAuthor('Test Author XML', 1);
+
 print_r($json);
--- src/GetBookList.php
+++ src/GetBookList.php
@@ -3,52 +3,63 @@
 namespace PhpCodeTest;
 
 use SimpleXMLElement;
+use Exception;
 
 class GetBookList
 {
     private $format;
 
-    public function __construct($format = 'json')
+    public function __construct(string $format = 'json')
     {
         $this->format = $format;
     }
 
-    public function getBooksByAuthor($authorName, $limit = 10)
+    public function getBooksByAuthor(string $authorName, int $limit = 10): array
     {
         $return = [];
 
-        $curl = curl_init();
-
-        curl_setopt(
-            $curl,
-            CURLOPT_URL,
-            "http://api.book-seller-example.com/by-author?q="
-                . $authorName
-                . '&limit='
-                . $limit
-                . '&format='
-                . $this->format
-        );
-        $output = curl_exec($curl);
-        curl_close($curl);
+        $output = $this->runCurl($authorName, $limit);
 
         if ($this->format == 'json') {
-            $json = json_decode($output);
-
-            foreach ($json as $result) {
-                $return[] = [
-                    'title' => $result->book->title,
-                    'author' => $result->book->author,
-                    'isbn' => $result->book->isbn,
-                    'quantity' => $result->stock->level,
-                    'price' => $result->stock->price,
-                ];
-            }
+            $return = $this->getArrayFromJsonString($output);
         } elseif ($this->format == 'xml') {
-            $xml = new SimpleXMLElement($output);
+            $return = $this->getArrayFromXmlString($output);
+        }
 
+        return $return;
+    }
+
+    protected function getArrayFromJsonString(string $string): array
+    {
+        $json = json_decode($string);
+
+        if (!$json) {
+            return [];
+        }
+
+        $array = [];
+        foreach ($json as $result) {
+            $array[] = [
+                'title' => $result->book->title,
+                'author' => $result->book->author,
+                'isbn' => $result->book->isbn,
+                'quantity' => $result->stock->level,
+                'price' => $result->stock->price,
+            ];
+        }
+
+        return $array;
+    }
+
+    protected function getArrayFromXmlString(string $string): array
+    {
+        try {
+            // For now just hope everything works, if not, return silently
+            $xml = new SimpleXMLElement($string);
+
+            $array = [];
             foreach ($xml as $result) {
-                $return[] = [
+                $array[] = [
                     'title' => $result->book['name'],
                     'author' => $result->book['author_name'],
                     'isbn' => $result->book['isbn_number'],
@@ -56,8 +67,46 @@ class GetBookList
                     'price' => $result->book->stock['unit_price'],
                 ];
             }
+        } catch (Exception $e) {
+            // Make sure we return correctly
+            return [];
         }
 
-        return $return;
+        return $array;
+    }
+
+    protected function getCurlUrl(): string
+    {
+        // return 'pgburton.com';
+        return "http://api.book-seller-example.com/by-author?q="
+            . $authorName
+            . '&limit='
+            . $limit
+            . '&format='
+            . $this->format;
+    }
+
+    protected function runCurl(string $authorName, int $limit): string
+    {
+        $curl = curl_init();
+
+        // Set the url
+        curl_setopt($curl, CURLOPT_URL, $this->getCurlUrl($authorName, $limit));
+        // Get curl to return the result not print it
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        // Store the output
+        $output = curl_exec($curl);
+
+        // Get the error so we can check how the exec went
+        $error = curl_error($curl);
+        curl_close($curl);
+
+        if ($error) {
+            // Return a valid array
+            return '';
+        }
+
+        return $output;
     }
 }
